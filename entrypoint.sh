3aa02c19d1c81a425c1ee88ec78233d0055c4136
jeremiah <git@jbutler.dev>
2021-02-24 13:35:21 +0000

    updates for docker command

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,4 +8,3 @@ echo $KEY
 echo "Running command step ca renew --daemon --exec "$RESTART_CMD" --ca-url "$CA_URL" --root "$ROOT" "$CERT" "$KEY""
 
 step ca renew --daemon --exec "$RESTART_CMD" --ca-url "$CA_URL" --root "$ROOT" "$CERT" "$KEY"
-#step ca renew --daemon --exec "docker restart nextcloud_proxy_1" /certs/cloud.jbutler.dev.crt /certs/cloud.jbutler.dev.key
